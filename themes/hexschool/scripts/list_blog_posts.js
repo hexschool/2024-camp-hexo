7c21a9a5c03337cb2beb261009223a20e48683a6
Jay <kred110396@gmail.com>
2024-04-04 02:33:36 +0800

    feat: implement feature to prevent image-induced layout breaks

--- themes/hexschool/scripts/list_blog_posts.js
+++ themes/hexschool/scripts/list_blog_posts.js
@@ -25,45 +25,47 @@ function listPostsHelper(posts, options) {
     if (style === 'list') {
         result += `<ul class="${ul_className}">`;
         posts.forEach(post => {
-            const title = post.title || post.slug;
-            const date = new Date(post.date).toLocaleDateString('zh-tw');
+          const imgRegex = /<img[^>]*>/g;
+          const content = post.content.replace(imgRegex, '');   // prevent image appear
+          const title = post.title || post.slug;
+          const date = new Date(post.date).toLocaleDateString('zh-tw');
 
-            result += `<li class="card border-0">
-              <article class="row g-0">
-                <div class="col-12 col-lg-4">
-                  <img
-                    src="${post.cover}"
-                    class="img-fluid w-100 h-100 object-fit-cover rounded-3"
-                    alt="article-${post.title}"
-                  >
-                </div>
-                <div class="col-12 col-lg-8">
-                  <div class="card-body pt-lg-0 pe-lg-0">
-                    <div class="d-flex justify-content-between justify-content-lg-start align-items-center gap-3 mb-3">
-                      <p class="card-text mb-0 fs-6 text-primary-600">
-                        <small class="text-body-secondary">
-                          ${date}
-                        </small>
-                      </p>`;
+          result += `<li class="card border-0">
+            <article class="row g-0">
+              <div class="col-12 col-lg-4">
+                <img
+                  src="${post.cover}"
+                  class="img-fluid w-100 h-100 object-fit-cover rounded-3"
+                  alt="article-${post.title}"
+                >
+              </div>
+              <div class="col-12 col-lg-8">
+                <div class="card-body pt-lg-0 pe-lg-0">
+                  <div class="d-flex justify-content-between justify-content-lg-start align-items-center gap-3 mb-3">
+                    <p class="card-text mb-0 fs-6 text-primary-600">
+                      <small class="text-body-secondary">
+                        ${date}
+                      </small>
+                    </p>`;
 
-            post.tags.forEach((tag) => {
-              result +=  `<span class="badge fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">
-                          ${tag.name}
-                        </span>`;
-            });
+          post.tags.forEach((tag) => {
+            result +=  `<span class="badge fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">
+                        ${tag.name}
+                      </span>`;
+          });
 
-            result +=  `</div>
-                    <h5 class="card-title mb-3 text-primary-700">
-                      ${title}
-                    </h5>
-                    <div class="cutoff-text mb-0 text-primary-700">
-                      ${post.content}
-                    </div>
-                    <a class="stretched-link" href="${url_for(post.path)}"></a>
+          result +=  `</div>
+                  <h5 class="card-title mb-3 text-primary-700">
+                    ${title}
+                  </h5>
+                  <div class="cutoff-text mb-0 text-primary-700">
+                    ${content}
                   </div>
+                  <a class="stretched-link" href="${url_for(post.path)}"></a>
                 </div>
-              </article>
-            </li>`;
+              </div>
+            </article>
+          </li>`;
         });
         result += '</ul>';
     }
